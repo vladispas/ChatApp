8409e3575abc66ae8555bee19cc3b97d1294d36b
Vlad Ispas <vlad.ispas@yahoo.com>
2023-01-28 11:41:53 +0200

    Refined project structure and added assets

---
+++ ChatApp/Helpers/Extensions/FontExtension.swift
@@ -0,0 +1,44 @@
+//
+//  FontExtension.swift
+//  ChatApp
+//
+//  Created by Vlad Ispas on 28.01.2023.
+//
+
+import Foundation
+import SwiftUI
+
+extension Font {
+    
+    public static var body: Font {
+        return Font.custom("LexendDeca-Regular", size: 14)
+    }
+    
+    public static var button: Font {
+        return Font.custom("LexendDeca-SemiBold", size: 14)
+    }
+    
+    public static var caption: Font {
+        return Font.custom("LexendDeca-Regular", size: 10)
+    }
+    
+    public static var tabBar: Font {
+        return Font.custom("LexendDeca-Regular", size: 12)
+    }
+    
+    public static var settings: Font {
+        return Font.custom("LexendDeca-Regular", size: 16)
+    }
+    
+    public static var title: Font {
+        return Font.custom("LexendDeca-Bold", size: 23)
+    }
+    
+    public static var pageTitle: Font {
+        return Font.custom("LexendDeca-SemiBold", size: 33)
+    }
+    
+    public static var chatHeading: Font {
+        return Font.custom("LexendDeca-SemiBold", size: 19)
+    }
+}
--- ChatApp/Views/RootView.swift
+++ ChatApp/Views/RootView.swift
@@ -1,5 +1,5 @@
 //
-//  ContentView.swift
+//  RootView.swift
 //  ChatApp
 //
 //  Created by Vlad Ispas on 28.01.2023.
@@ -7,20 +7,21 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct RootView: View {
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
             Text("Hello, world!")
+                .font(Font.title)
         }
         .padding()
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
+struct RootView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        RootView()
     }
 }
