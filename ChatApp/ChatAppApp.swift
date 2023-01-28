a4deb32d79ccadeb78c01e118b429d0071fd839f
Vlad Ispas <vlad.ispas@yahoo.com>
2023-01-28 10:28:09 +0200

    First commit

---
+++ ChatApp/ChatAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatAppApp.swift
+//  ChatApp
+//
+//  Created by Vlad Ispas on 28.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct ChatAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
